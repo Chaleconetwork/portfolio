7b8eca7e760c950ea89f9386d7d222acfe938c6b
chaleconetwork <chaleconetworking@gmail.com>
2022-12-06 23:23:51 -0300

    new portfolio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Fragment } from 'react'
+import Sidebar from '../components/Sidebar'
+import styles from '../styles/App.module.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <Fragment>
+            <Sidebar />
+            <Component {...pageProps} />
+        </Fragment>
+    )
 }
